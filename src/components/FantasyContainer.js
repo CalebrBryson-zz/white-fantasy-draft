a29cf173d758d7e5c51d28f460579d4be1e09713
Caleb Bryson <calebrbryson@gmail.com>
2019-08-26 16:05:40 -0600

    Displaying static server data

--- src/components/FantasyContainer.js
+++ src/components/FantasyContainer.js
@@ -1,9 +1,20 @@
 import React, {useState, useEffect} from "react";
+import axios from 'axios';
 
 export default function FantasyContainer() {
+
+  const [players, setPlayers] = useState([]);
+
+  useEffect(()=> {
+    axios.get('https://fantasy-backend.herokuapp.com/api/players').then(data => {return setPlayers(data)});
+  },[]);
+
+
+
+
   return (
     <div>
-      <span>What is this</span>
+      <span>{JSON.stringify(players)}</span>
     </div>
   );
 }
